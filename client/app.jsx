13768f31f560212bb4b910daee1b596f0a8aac00
Jacob Husband <jakehusband2@gmail.com>
2022-11-08 01:53:16 +0000

    Add splash page component to final project

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Home from './pages/home';
+import RunnerFuze from './pages/splash';
 
 export default class App extends React.Component {
   render() {
-    return <Home />;
+    return <RunnerFuze />;
   }
 }
---
+++ client/pages/splash.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Splash extends React.Component {
+
+  render() {
+    return (
+      <div className='container'>
+        <div className='radial-gradient-overlay' />
+        <h1>RunnerFuze</h1>
+      </div>
+    );
+  }
+}
