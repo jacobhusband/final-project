90f0ca98f180f53124ce46268b3c483dd2a87f14
Jacob Husband <jakehusband2@gmail.com>
2022-11-08 20:14:46 +0000

    Add webcam feed to photo page

--- client/pages/photo.jsx
+++ client/pages/photo.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import Navbar from '../components/navbar';
+import Webcam from 'react-webcam';
 
 export default class Photo extends React.Component {
+
+  WebcamCapture() {
+    return <Webcam
+      audio={false}
+      screenshotFormat="image/jpeg"
+    >
+      {({ getScreenshot }) => (
+        <button
+          onClick={() => {
+            getScreenshot();
+          }}
+        >
+          Capture photo
+        </button>
+      )}
+    </Webcam>;
+  }
 
   render() {
     return (
       <div className='text-center'>
         <Navbar home={this.props.home} />
         <p className='lh-lg h4 fw-bold mt-5'>Take a pre-exercise photo</p>
-        <div className='camera-container' />
+        <div className='camera-container'>
+          {this.WebcamCapture()}
+        </div>
       </div>
     );
   }
