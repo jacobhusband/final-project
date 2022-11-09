803e60f39056e1cbd8b8dc8bf9959b41d1464128
Jacob Husband <jakehusband2@gmail.com>
2022-11-09 23:42:22 +0000

    Make seconds look like clock time

--- client/components/stopwatch.jsx
+++ client/components/stopwatch.jsx
@@ -1,24 +1,106 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import Message from '../components/message';
 
 export default class Stopwatch extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      time: 0
+      time: 0,
+      id: null,
+      phase: 'start'
     };
+    this.setTimeInterval = this.setTimeInterval.bind(this);
+    this.increaseTime = this.increaseTime.bind(this);
+    this.pauseTime = this.pauseTime.bind(this);
+    this.stopRun = this.stopRun.bind(this);
+    this.endRun = this.endRun.bind(this);
+  }
+
+  setTimeInterval() {
+    this.setState({
+      id: setInterval(this.increaseTime, 1000),
+      phase: 'during'
+    });
+  }
+
+  increaseTime() {
+    const time = this.state.time;
+    this.setState({
+      time: time + 1
+    });
+  }
+
+  pauseTime() {
+    clearInterval(this.state.id);
+    this.setState({
+      id: null,
+      phase: 'paused'
+    });
+  }
+
+  stopRun() {
+    clearInterval(this.state.id);
+    this.setState({
+      id: null,
+      phase: 'finished'
+    });
+  }
+
+  endRun() {
+
+  }
+
+  modifyTime(time) {
+    const seconds = time % 60;
+    const minutes = Math.trunc(time / 60);
+    const hours = Math.trunc(minutes / 60);
+    let s = seconds.toString();
+    let m = minutes.toString();
+    let h = hours.toString();
+    if (s.length < 2) {
+      s = '0' + s;
+    }
+    if (m.length < 2) {
+      m = '0' + m;
+    }
+    if (h.length < 2) {
+      h = '0' + h;
+    }
+    return [h, m, s].join(':');
   }
 
   render() {
+    let buttonLeft, buttonRight, message;
+    if (this.state.phase === 'start') {
+      message = 'ready';
+      buttonLeft = <Button onClick={this.setTimeInterval}>Start</Button>;
+      buttonRight = <Button onClick={this.stopRun}>Stop</Button>;
+    } else if (this.state.phase === 'during') {
+      message = 'time';
+      buttonLeft = <Button onClick={this.pauseTime}>Pause</Button>;
+      buttonRight = <Button onClick={this.stopRun}>Stop</Button>;
+    } else if (this.state.phase === 'paused') {
+      message = 'time';
+      buttonLeft = <Button onClick={this.setTimeInterval}>Resume</Button>;
+      buttonRight = <Button onClick={this.stopRun}>Stop</Button>;
+    } else if (this.state.phase === 'finished') {
+      message = 'final';
+      buttonLeft = <Button onClick={this.setTimeInterval}>Resume</Button>;
+      buttonRight = <Button onClick={this.endRun}>Finish</Button>;
+    }
+
+    const time = this.modifyTime(this.state.time);
 
     return (
       <>
+        <Message message={message} />
         <div className='stopwatch d-flex justify-content-center align-items-center mx-auto border border-3 border-primary rounded-circle'>
-          <p className='h1 text-primary'>{this.state.time}</p>
+          <p className='h1 text-primary'>{time}</p>
         </div>
         <div className="d-flex justify-content-center mt-3 gap-2">
-          <Button>Start</Button>
-          <Button>Stop</Button>
+          {buttonLeft}
+          {buttonRight}
         </div>
       </>
     );
--- client/pages/timer.jsx
+++ client/pages/timer.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import Navbar from '../components/navbar';
-import Message from '../components/message';
 import Stopwatch from '../components/stopwatch';
 
 export default class Timer extends React.Component {
   render() {
-    const message = 'ready';
-
     return (
       <>
         <Navbar />
-        <Message message={message} />
         <Stopwatch />
       </>
     );
